043771f2181d6f02af432406bfe82be294667747
Laura Hanna <lhanna302@gmail.com>
2021-05-02 14:20:13 -0400

    Beginning implementation

--- challenge/src/main/java/com/framework/solution/service/Library.java
+++ challenge/src/main/java/com/framework/solution/service/Library.java
@@ -2,6 +2,8 @@ package com.framework.solution.service;
 
 import com.framework.solution.enums.ContentType;
 import com.framework.solution.model.Content;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
@@ -9,30 +11,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class Library implements ILibrary {
 
+  List<Content> initialContent; // some way to read a JSON file
+  List<Content> allContent;
   // Adds Content to the Library, not the JSON file
   @Override
   public Content addContent(Content content) {
-    return null;
+    allContent.add(content);
+    return content;
   }
 
   // Deletes Content from the Library, not the JSON file
   @Override
   public void deleteContentById(UUID id) {
-
+    for(Content content : allContent)
+      if(content.getContentId() == id){
+        allContent.remove(content);
+      }
   }
 
   @Override
   public int countContentByType(ContentType contentType) {
-    return 0;
+    return this.getContentsByType(contentType).size();
   }
 
   @Override
   public List<Content> getAllContents() {
-    return null;
+    return allContent;
   }
 
   @Override
   public List<Content> getContentsByType(ContentType contentType) {
-    return null;
+    List<Content> contentsByType = new ArrayList<Content>();
+    // There's a findAny() but not a findAll()?
+    for(Content content : allContent)
+      if(content.getContentType().equals(contentType)) {
+        contentsByType.add(content);
+      }
+    return contentsByType;
   }
 }
