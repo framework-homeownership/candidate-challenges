d52390b92d2c8c097a4b02e183e275526008bed8
Laura Hanna <lhanna302@gmail.com>
2021-05-02 16:28:41 -0400

    Forgot how removing from lists works in Java

--- challenge/src/main/java/com/framework/solution/service/Library.java
+++ challenge/src/main/java/com/framework/solution/service/Library.java
@@ -10,6 +10,7 @@ import com.framework.solution.model.Content;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,7 +25,7 @@ public class Library implements ILibrary {
 
   public Library(){
     this.initialContent = seedInitialContent();
-    allContent.addAll(this.initialContent);
+    this.allContent.addAll(this.initialContent);
   }
 
   private List<Content> seedInitialContent(){
@@ -45,16 +46,17 @@ public class Library implements ILibrary {
   // Adds Content to the Library, not the JSON file
   @Override
   public Content addContent(Content content) {
-    allContent.add(content);
+    this.allContent.add(content);
     return content;
   }
 
   // Deletes Content from the Library, not the JSON file
   @Override
   public void deleteContentById(UUID id) {
-    for(Content content : allContent)
-      if(content.getContentId() == id){
-        allContent.remove(content);
+    Iterator<Content> iterator = allContent.iterator();
+    while(iterator.hasNext())
+      if(iterator.next().getContentId().equals(id)){
+        iterator.remove();
       }
   }
 
@@ -65,14 +67,14 @@ public class Library implements ILibrary {
 
   @Override
   public List<Content> getAllContents() {
-    return allContent;
+    return this.allContent;
   }
 
   @Override
   public List<Content> getContentsByType(ContentType contentType) {
     List<Content> contentsByType = new ArrayList<Content>();
     // There's a findAny() but not a findAll()?
-    for(Content content : allContent)
+    for(Content content : this.allContent)
       if(content.getContentType().equals(contentType)) {
         contentsByType.add(content);
       }
--- challenge/src/test/java/com/framework/solution/ChallengeApplicationTests.java
+++ challenge/src/test/java/com/framework/solution/ChallengeApplicationTests.java
@@ -1,5 +1,7 @@
 package com.framework.solution;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +15,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChallengeApplicationTests {
 	
+	Library getSeededLibrary(){
+		Library library = new Library();
+		List<Content> libraryContent = library.getAllContents();
+		assert((libraryContent.size())== 6);
+		return library;
+	}
+
 	@Test
 	void contextLoads() {
 	}
@@ -20,18 +29,31 @@ class ChallengeApplicationTests {
 	@Test
 	void getAllContents(){
 		//String evictedId = "338e2efc-8749-11eb-8dcd-0242ac130003"
-		Library library = new Library();
-		List<Content> libraryContent = library.getAllContents();
-		assert((libraryContent.size())== 6);
+		getSeededLibrary();
 	}
 
 	@Test
 	void addContent(){
-		Library library = new Library();
-		assert(library.getAllContents().size() == 6);
+		Library library = getSeededLibrary();
 
 		library.addContent(new Content(new UUID(0, 100), "The Return of the King", ContentType.FANTASY));
 		assert(library.getAllContents().size() == 7);
+	}
+
+	@Test
+	void deleteContent(){
+		Library library = getSeededLibrary();
+		UUID testUUID = UUID.fromString("5a33d150-81eb-11eb-8dcd-0242ac130003");
+		library.deleteContentById(testUUID);
+
+		//assert(library.getAllContents().size() == 5);
+		List<Content> alteredContent = library.getAllContents();
+		System.out.println(alteredContent);
+		for(Content content : library.getAllContents()){
+			if(content.getContentId().equals(testUUID)){
+				fail("testUUID " + testUUID + " found despite delete");
+			}
+		}
 	}
 
 }
